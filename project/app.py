b40a651c1d445c01bc4bd5645d654c4f77f82677
murial <mukmin19ti@mahasiswa.pcr.ac.id>
2023-06-13 10:36:21 +0700

    UI partly done
    upload success, displaying image list, displaying 3D canvas

--- project/app.py
+++ project/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, jsonify, render_template, request, flash, redirect, session
 from werkzeug.utils import secure_filename
 from PIL import Image
 import os
 
 app = Flask(__name__)
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 imageList = os.listdir('project/static/images/pbr')
 imagelist = [image for image in imageList]
@@ -12,34 +14,19 @@ UPLOAD_FOLDER = 'project/static/images/original/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['POST', 'GET'])
 def main():
-    
+        
     return render_template('index.html', imagelist=imagelist)
 
+@app.route('/upload', methods=['POST', 'GET'])
 def upload():
-    # if request.method == 'POST':
-    #     if 'image' in request.files:
-    #         image = request.files['image']
-    #         image.save('project/static/images/original/' + 'original.png')
-    #         image_filename = 'project/static/images/original/original.png'
-    #         return render_template('index.html', image_filename=image_filename, imagelist=imagelist)
-
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'images' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['image']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected image')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            # filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'original.png'))
-
+    
+    file = request.files['image']
+    fn = secure_filename(file.filename)
+    file.save(os.path.join(UPLOAD_FOLDER, 'original.png'))  # replace FILES_DIR with your own directory
+    image_filename = os.path.join(UPLOAD_FOLDER, 'original.png')
+    return render_template('index.html', image_filename=image_filename)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
--- project/static/js/main.js
+++ project/static/js/main.js
@@ -6,19 +6,35 @@ var renderer = new THREE.WebGLRenderer();
 // Set the background color to grey
 renderer.setClearColor(0x888888);
 
+function getWidth() {
+    return parseInt(window.getComputedStyle(canvas).width);
+}
+  
+function getHeight() {
+    return parseInt(window.getComputedStyle(canvas).height);
+}
+
 // Set the size of the renderer to match the container
 renderer.setSize(container.offsetWidth, container.offsetHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 container.appendChild(renderer.domElement);
 
 //make it responsive (dynamic size)
-window.addEventListener('resize', function()
-{
-	var width = window.innerWidth;
-	var height = window.innerHeight;
-	renderer.setSize(width,height);
-	camera.aspect = width/height;
-	camera.updateProjectionMatrix();
-});
+// window.addEventListener('resize', function()
+// {
+// 	var width = window.innerWidth;
+// 	var height = window.innerHeight;
+// 	renderer.setSize(width,height);
+// 	camera.aspect = width/height;
+// 	camera.updateProjectionMatrix();
+// });
+
+addEventListener("resize",() => {
+    camera.aspect = getWidth() / getHeight();
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+},false);
 
 var scene = new THREE.Scene();
 
@@ -47,76 +63,73 @@ function animate() {
 
 animate();
 
-// ------------------------------------------UPLOAD SECTION-------------------------------------
+// ----------------------------------UPLOAD SECTION----------------------------
 
-document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-	const dropZoneElement = inputElement.closest(".drop-zone");
-
-	dropZoneElement.addEventListener("click", (e) => {
-		inputElement.click();
-	});
-
-	inputElement.addEventListener("change", (e) => {
-		if (inputElement.files.length) {
-			updateThumbnail(dropZoneElement, inputElement.files[0]);
-		}
-	});
-
-	dropZoneElement.addEventListener("dragover", (e) => {
-		e.preventDefault();
-		dropZoneElement.classList.add("drop-zone--over");
-	});
-
-	["dragleave", "dragend"].forEach((type) => {
-		dropZoneElement.addEventListener(type, (e) => {
-			dropZoneElement.classList.remove("drop-zone--over");
-		});
-	});
-
-	dropZoneElement.addEventListener("drop", (e) => {
-		e.preventDefault();
-
-		if (e.dataTransfer.files.length) {
-			inputElement.files = e.dataTransfer.files;
-			updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-		}
-
-		dropZoneElement.classList.remove("drop-zone--over");
-	});
+// prevent the default behavior of web browser
+['dragleave', 'drop', 'dragenter', 'dragover'].forEach(function (evt) {
+    document.addEventListener(evt, function (e) {
+        e.preventDefault();
+    }, false);
 });
 
-/**
- * Updates the thumbnail on a drop zone element.
- *
- * @param {HTMLElement} dropZoneElement
- * @param {File} file
- */
-function updateThumbnail(dropZoneElement, file) {
-	let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+var drop_area = document.getElementById('drop_area');
+drop_area.addEventListener('drop', function (e) {
+    e.preventDefault();
+    var fileList = e.dataTransfer.files; // the files to be uploaded
+    if (fileList.length == 0) {
+        return false;
+    }
 
-	// First time - remove the prompt
-	if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-		dropZoneElement.querySelector(".drop-zone__prompt").remove();
-	}
+    // we use XMLHttpRequest here instead of fetch, because with the former we can easily implement progress and speed.
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', '/upload', true); // aussume that the url /upload handles uploading.
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // uploading is successful
+            alert('Successfully uploaded!');  // please replace with your own logic
+            reloadImage();
+        }
+    };
 
-	// First time - there is no thumbnail element, so lets create it
-	if (!thumbnailElement) {
-		thumbnailElement = document.createElement("div");
-		thumbnailElement.classList.add("drop-zone__thumb");
-		dropZoneElement.appendChild(thumbnailElement);
-	}
+    // show uploading progress
+    var lastTime = Date.now();
+    var lastLoad = 0; 
+    xhr.upload.onprogress = function (event) {
+        if (event.lengthComputable) {
+            // update progress
+            var percent = Math.floor(event.loaded / event.total * 100);
+            document.getElementById('upload_progress').textContent = percent + '%';
 
-	thumbnailElement.dataset.label = file.name;
+            // update speed
+            var curTime = Date.now();
+            var curLoad = event.loaded;
+            var speed = ((curLoad - lastLoad) / (curTime - lastTime) / 1024).toFixed(2);
+            document.getElementById('speed').textContent = speed + 'MB/s'
+            lastTime = curTime;
+            lastLoad = curLoad;
+        }
+    };
 
-	// Show thumbnail for image files
-	if (file.type.startsWith("original")) {
-		const reader = new FileReader();
+    // send files to server
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    var fd = new FormData();
+    for (let file of fileList) {
+        fd.append('image', file);
+    }
+    lastTime = Date.now();
+    xhr.send(fd);
+}, false);
 
-		reader.readAsDataURL(file);
-		reader.onload = () => {
-			thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-		};
-	} else {
-		thumbnailElement.style.backgroundImage = null;
-	}
+
+// -----------------------------refresh certain content---------------
+
+function reloadImage() {
+    var imageElement = document.getElementById('picture');
+
+    // Add a timestamp query parameter to the image URL to force reload
+    // var timestamp = new Date().getTime();
+    var imageUrl = imageElement.src + '?t=';
+
+    // Set the updated URL to the image element
+    imageElement.src = imageUrl;
 }
